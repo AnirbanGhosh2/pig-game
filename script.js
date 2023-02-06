ed82b58a96e93c85bdbd7bad284e3d7fd6eefc9c
Anirban-Ghosh <110912334+AnirbanGhosh2@users.noreply.github.com>
2023-02-06 21:19:17 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,5 @@
 'use strict';
-alert("Alert! You are going to check your luck. the rule of the games are
-      Roll the dice: The current player has to roll the dice and then a random number will be generated. If current player gets any number other than 1 on the dice then that number will be added to the current score (initially the current score will be 0) and then the new score will be displayed under Current Score section.  
-    Note: If the current player gets 1 on the dice then the players will be switched i.e. the current player will become in-active and vice-versa.
-    Hold: If the current player clicks on HOLD, then the Current Score will be added to the Total Score. When the active player clicks the Hold button then the total score is evaluated. If the Total Score >= 100 then the current player wins else the players are switched.");
+alert("Alert! You are going to check your luck. the rule of the games are.Roll the dice: The current player has to roll the dice and then a random number will be generated. If current player gets any number other than 1 on the dice then that number will be added to the current score (initially the current score will be 0) and then the new score will be displayed under Current Score section.  Note: If the current player gets 1 on the dice then the players will be switched i.e. the current player will become in-active and vice-versa.Hold: If the current player clicks on HOLD, then the Current Score will be added to the Total Score. When the active player clicks the Hold button then the total score is evaluated. If the Total Score >= 100 then the current player wins else the players are switched.");
 const player0el = document.querySelector('.player--0');
 const player1el = document.querySelector('.player--1');
 const score0 = document.querySelector('#score--0');
